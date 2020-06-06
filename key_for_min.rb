e6d0cf6932f8e5ae7388d8d344c2391d0fad2866
Matt Green <magreen118@gmail.com>
2020-06-06 05:04:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,10 @@ def key_for_min_value(name_hash)
     return nil
   else
     name_hash.each do |key, value|
-      
+      if value < lowest_value
+        lowest_value = value
+        lowest_key = key
+      end
     end
   end
   return lowest_key
